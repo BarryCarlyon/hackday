e105cc72644f5c701dacb0cb69e5d42bcd3ecf44
Barry Carlyon <barrycarlyon@Linsey.(none)>
2009-05-10 09:04:28 +0100

    List style

--- templates/home.php
+++ templates/home.php
@@ -6,9 +6,11 @@ $this->content_title = 'Dumb Deeds';
 $this->page_content = '
 
 <p>
-
 This is where you type content, its govered by the content management systsm
-
 </p>
+
+<ul>
+        <li>List Age</li>
+</ul>
 
 ';
