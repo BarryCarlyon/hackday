7630fd7a8aad00e15e78899cfd4a6a9bc39356bd
Barry Carlyon <barrycarlyon@Linsey.(none)>
2009-05-10 09:10:21 +0100

    look who cant remember who to use his cms

--- templates/home.php
+++ templates/home.php
@@ -3,7 +3,7 @@
 $this->page_title = 'Home';
 $this->content_title = 'Dumb Deeds';
 
-$this->page_contents = '
+$this->prehtml = '
 
 <p>
 This is where you type content, its govered by the content management systsm
