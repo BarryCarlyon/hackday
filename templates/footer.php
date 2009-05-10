c83bcb6fb38dff6e44bc288b2e1ede20c042223d
Barry Carlyon <barrycarlyon@Linsey.(none)>
2009-05-10 09:13:59 +0100

    Fixing cms stuff

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,10 @@
+<?php
+
+$this->html .= '
         <div class="blocker"></div>
 </div>
 
 </body>
 </html>
+';
+
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <?php
 
-$this->prehtml .= '
+$this->html = '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-gb" xml:lang="en-gb">
 <head profile="http://gmpg.org/xfn/11">
