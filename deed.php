4bffb4b464a8fd621584d3b2fa6279bf082e4e72
BarryCarlyon <barry@barrycarlyon.co.uk>
2011-08-06 12:09:36 +0100

    oops

--- deed.php
+++
@@ -1,9 +0,0 @@
-<h2>Deed Title</h2>
-
-<small><?php echo time(); ?></small>
-
-<?php if (rand(1,2) == 1): ?>
-    <img src="http://localhost/image_block.png" height='50px' />
-<?php else: ?>
-    <p>Something happens here</p>
-<?php endif ?>
